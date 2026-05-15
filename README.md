02a2a97df2890eb5944a4b9e799c57e5e49f4364
Elyrene <3222956277@qq.com>
2026-05-15 23:09:57 +0800

    🎉 init: 图书馆项目初始化

---
+++ README.md
@@ -0,0 +1,48 @@
+# 下载依赖
+
+下载代码格式化工具
+
+```bash
+npm istall
+```
+
+下载后端依赖
+
+```bash
+cd serve
+npm install
+```
+
+下载前端依赖
+
+```bash
+cd web
+npm install
+```
+
+# 启动项目
+
+这里包含启动前端与后端的指令
+
+## 启动后端
+
+运行以下代码启动后端
+
+```bash
+cd serve
+npm run dev
+```
+
+构建指令
+
+```bash
+npm run build
+```
+
+运行指令
+
+```bash
+npm run serve
+```
+
+## 启动前端
---
+++ web/vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+
+const HOST = '127.0.0.1';
+const PORT = 5600;
+
+export default defineConfig({
+  // server:{
+  //   proxy:{
+  //     "^/api/.*":"http://localhost:3000",
+  //   }
+  // },
+  server:{
+    port: PORT,
+    strictPort: true,
+    host: HOST,
+	// hmr: {
+	// 	protocol: 'ws',
+	// 	// host: HOST,
+	// 	port: PORT + 1,
+	// },
+  },
+  build: {
+    // 打包输出到 dist 目录，server.js 会将此目录作为静态根目录
+    outDir: 'dist',
+    emptyOutDir: true, 
+    // 入口文件
+    rollupOptions: {
+     input: 'index.html', // 将根目录的 index.html 作为入口
+    }
+  }
+});
