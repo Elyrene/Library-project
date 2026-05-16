9888ae8a59fdfadd5ffba55135dbdefb5133b8c9
Elyrene <3222956277@qq.com>
2026-05-16 18:04:32 +0800

    🔧 build: 添加 biome 代码检查插件

--- serve/src/index.ts
+++ serve/src/index.ts
@@ -1,14 +1,14 @@
 // 引入 Node.js 内置的 http 模块，测试 @types/node 是否生效
-import http from 'http';
+import http from "node:http";
 
 // 测试 TS 类型推断
 const port: number = 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, Full-Stack TypeScript!');
+	res.writeHead(200, { "Content-Type": "text/plain" });
+	res.end("Hello, Full-Stack TypeScript!");
 });
 
 server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`) ;
+	console.log(`Server is running on http://localhost:${port}`);
 });
